d155c34bc69a22153665490afc440a9f78108242
Duff72 <duffy.andd@gmail.com>
2025-03-19 13:18:20 -0400

    added todo list

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ addButton.addEventListener("click", function (event) {
   const newItemText = document.getElementById("addItem").value;
   if (RegExp("^[a-zA-Z]+$").test(newItemText)) {
     const li = document.createElement("li");
-    li.innerHTML = `<button style = "background: none" class = "listItem">${newItemText}</button>`;
+    li.innerHTML = `<button style = "background: none; border: none" class = "listItem">${newItemText}</button>`;
     li.addEventListener("click", function (event) {
       event.preventDefault();
-      if (li.firstChild.style.color != "red") li.firstChild.style.color = "red";
+      if (li.firstChild.style.textDecoration != "line-through")
+        li.firstChild.style.textDecoration = "line-through";
       else {
-        li.firstChild.style.color = "black";
+        li.firstChild.style.textDecoration = "none";
       }
     });
 
@@ -36,4 +37,63 @@ alphabetize.addEventListener("click", function (event) {
 clearBtn.addEventListener("click", function (event) {
   const allListItems = document.querySelectorAll("li");
   allListItems.forEach((item) => item.remove());
+});
+
+const registerButton = document.getElementById("registerButton");
+
+const errorBox = document.getElementById("errorDisplay");
+
+function showError(message) {
+  errorBox.innerHTML = message;
+  errorBox.style.display = "flex";
+}
+
+registerButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const usernameRegex = new RegExp(
+    "^(?=.{4,})(?=(?:.*([A-Za-z0-9])))[A-Za-z0-9]+$"
+  );
+  const emailRegex = new RegExp(
+    "^(?!.*@example.com$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
+  );
+  const passwordRegex = new RegExp(
+    "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
+  );
+
+  if (!usernameRegex.test(document.getElementById("usernameField").value)) {
+    showError("Invalid username");
+  } else {
+    localStorage.setItem(
+      "username",
+      `${document.getElementById("usernameField").value.toLowerCase()}`
+    );
+  }
+
+  if (!emailRegex.test(document.getElementById("emailField").value)) {
+    showError("Invalid email address");
+  } else {
+    localStorage.setItem(
+      "email",
+      `${document.getElementById("emailField").value.toLowerCase()}`
+    );
+  }
+
+  if (!passwordRegex.test(document.getElementById("passwordField").value)) {
+    showError("Invalid password");
+  } else {
+    localStorage.setItem(
+      "password",
+      `${document.getElementById("passwordField").value}`
+    );
+  }
+
+  if (
+    document.getElementById("passwordField").value !=
+    document.getElementById("passwordCheckField").value
+  ) {
+    showError("Passwords do not match");
+  }
+  if (document.getElementById("terms").checked == false) {
+    showError("You must agree to the Terms of Use");
+  }
 });
