0b922554d0f109ddf9067f09ec4ee4b4181909a1
Duff72 <duffy.andd@gmail.com>
2025-03-19 13:45:15 -0400

    added BOM, did some styling, added some comments

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ const addButton = document.getElementById("addButton");
 const alphabetize = document.getElementById("alphabetize");
 const clearBtn = document.getElementById("clearBtn");
 const listItem = document.querySelectorAll(".listItem");
+const openBtn = document.getElementById("openWindow");
+const closeBtn = document.getElementById("closeWindow");
 
 addButton.addEventListener("click", function (event) {
   event.preventDefault();
   const newItemText = document.getElementById("addItem").value;
   if (RegExp("^[a-zA-Z]+$").test(newItemText)) {
     const li = document.createElement("li");
-    li.innerHTML = `<button style = "background: none; border: none" class = "listItem">${newItemText}</button>`;
+    li.innerHTML = `<button style = "background: none; border: none" class = "listItem">${newItemText}</button>`; // creates a button in the list with no styling
     li.addEventListener("click", function (event) {
+      // adds event listener to each list item to (un)strikethrough
       event.preventDefault();
       if (li.firstChild.style.textDecoration != "line-through")
         li.firstChild.style.textDecoration = "line-through";
@@ -21,22 +24,32 @@ addButton.addEventListener("click", function (event) {
     });
 
     list.appendChild(li);
-    document.getElementById("addItem").value = "";
+    document.getElementById("addItem").value = ""; //clears the input after adding the item
   } else {
-    alert("Letters only please!");
+    alert("Letters only please!"); //throws an alert if non-letters are entered
   }
 });
 
 alphabetize.addEventListener("click", function (event) {
   event.preventDefault();
-  const listItems = Array.from(list.getElementsByTagName("li"));
-  listItems.sort((a, b) => a.textContent.localeCompare(b.textContent));
-  listItems.forEach((li) => list.appendChild(li));
+  const listItems = Array.from(list.getElementsByTagName("li")); //creates array from list items
+  listItems.sort((a, b) => a.textContent.localeCompare(b.textContent)); // sorts them alphabetically
+  listItems.forEach((li) => list.appendChild(li)); // rearranges list
 });
 
 clearBtn.addEventListener("click", function (event) {
   const allListItems = document.querySelectorAll("li");
-  allListItems.forEach((item) => item.remove());
+  allListItems.forEach((item) => item.remove()); // clears the list
+});
+
+openBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.open("https://www.google.com/"); // opens a new google window
+});
+
+closeBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.close(); //closes the window
 });
 
 const registerButton = document.getElementById("registerButton");
